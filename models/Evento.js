4f3777ea197a037c13e357af6725f12dc196d5c5
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2023-10-21 20:44:09 +0200

    Added 'Evento' mongoose Schema

--- models/Evento.js
+++ models/Evento.js
@@ -4,21 +4,24 @@ const { Schema, model } = require("mongoose");
 const EventoSchema = Schema({
     title: {
         type: String, 
-        require: true
+        required: true
     },
     start: {
-        type: Date
+        type: Date,
+        required: true
     },
     notes: {
         type: String
     },
     end: {
-        type: Date
+        type: Date,
+        required: true
     },
     // Nombre del otro esquema.
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'Usuario'
+        ref: 'Usuario',
+        required: true
     }
 
 }); 
