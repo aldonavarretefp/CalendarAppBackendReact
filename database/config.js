0a3e4bd4ca1fd5f37d5acf0e046ba816f488e616
Aldo Navarrete <aldo.navarrete@fi.unam.edu>
2023-11-05 21:29:58 +0100

    Modified db connection

--- database/config.js
+++ database/config.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.DB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Conectado a la base de datos");
+        // await mongoose.connect(process.env.DB_CNN, {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true
+        // });
+        await mongoose.connect(process.env.DB_CNN);
+        console.log("Conectando a la base de datos...");
     } catch (error) {
-        console.log(error);
-        throw new Error("Error al conectar la base de datos");
+        throw error;
+    } finally {
+        console.log("Conectado a la base de datos.");
     }
 }
 
